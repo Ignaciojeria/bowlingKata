5b4f502eda2308e0a9c491f523c4bf1b1d38410a
Ignacio <you@example.com>
2018-08-11 15:04:06 -0400

    first commit

---
+++ src/main/java/com/bolos/BolosApplication.java
@@ -0,0 +1,12 @@
+package com.bolos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolosApplication.class, args);
+	}
+}
